9c0b1629223ad986e73ab43644d5b7bb9c3102c5
rodrigoluis <rodrigoluis@gmail.com>
2024-10-27 18:23:19 -0300

    Scripts added

---
+++ js/participantes.js
@@ -0,0 +1,111 @@
+   //----------------------------- COORDENADOR -------------------------
+   const perfilCoord = [{
+      imgUrl: './img/orientadorRodrigo.jpeg', 
+      nome: 'Rodrigo Luis de Souza da Silva', 
+      projeto: 'Todos',
+      lattesUrl: 'http://lattes.cnpq.br/6878718102770367', 
+   }];
+
+
+   //----------------------------- PROFESSORES COLABORADORES -------------------------
+   const perfisProfColab = [
+   { imgUrl: './img/professorCaniato.jpeg', 
+   nome: 'Marcelo Caniato', 
+   projeto: 'ELoS',
+   lattesUrl: 'http://lattes.cnpq.br/9704187911798485'},
+
+   { imgUrl: './img/professorKnop.jpeg', 
+   nome: 'Igor Knop', 
+   projeto: 'ELoS',
+   lattesUrl: 'http://lattes.cnpq.br/9170658424711012' },
+
+   { imgUrl: "./img/professorLuizMaurilio.jpg", 
+   nome: 'Luiz Maurílio',
+   projeto: 'FisioVR', 
+   lattesUrl: 'http://lattes.cnpq.br/4491455337486151' },
+
+   { imgUrl: './img/professorPagani.jpg', 
+   nome: 'Eduardo Pagani', 
+   projeto: 'Recyclos',
+   lattesUrl: 'http://lattes.cnpq.br/2722036803654293' },
+   // Adicione mais perfis aqui
+   ];   
+
+
+   //--------------------------- ALUNOS ---------------------------------
+   const perfisAlunos = [
+   { imgUrl: './img/lara.png', 
+   nome: 'Lara Gonçalves de Souza', 
+   projeto: 'ELoS',
+   lattesUrl: 'null', 
+   linkedinUrl: 'https://www.linkedin.com/in/lara-goncalvs' },
+
+   { imgUrl: './img/leticiaElos.jpeg', 
+   nome: 'Letícia Miyuki Ferreira Yamashita', 
+   projeto: 'ELoS',
+   lattesUrl: 'http://lattes.cnpq.br/0366730628007428', 
+   linkedinUrl: 'https://www.linkedin.com/in/let%C3%ADcia-miyuki-yamashita-8b56b4293/' },
+
+   { imgUrl: './img/marcosElos.jpeg', 
+   nome: 'Marcos Vinícius Santos de Lima', 
+   projeto: 'ELoS',
+   lattesUrl: 'https://lattes.cnpq.br/7756604103041345', 
+   linkedinUrl: 'https://www.linkedin.com/in/marcos-vinixius/' },
+   // Adicione mais perfis aqui
+   ];
+
+
+   //--------------------------- VOLUNTÁRIOS ---------------------------------
+   const perfisVoluntarios = [
+   { imgUrl: './img/niltoRecyclos.jpeg', 
+   nome: 'Nilton Teixeira', 
+   projeto: 'Recyclos',
+   lattesUrl: 'http://lattes.cnpq.br/8963145201490611', 
+   linkedinUrl: 'https://www.linkedin.com/in/nilton-teixeira-182790255/' },
+   ];   
+
+
+
+//--------------------------- EGRESSOS ---------------------------------
+const perfisEgressos = [
+   {
+      imgUrl: './img/danielFisioVR.jpeg',
+      nome: 'Daniel Muller Rezende',
+      projeto: 'FisioVR',
+      lattesUrl: 'http://lattes.cnpq.br/9286013999555117', 
+      linkedinUrl: 'https://www.linkedin.com/in/daniel-muller-rezende-ab2542222'
+   },
+
+   {
+      imgUrl: './img/rodrigoElos.jpeg',
+      nome: 'Rodrigo Oliveira Ferrari',
+      projeto: 'ELoS',
+      lattesUrl: 'http://lattes.cnpq.br/1670593873823116 ', 
+      linkedinUrl: 'https://www.linkedin.com/in/rodrigo-oliveira-ferrari-b842a8255/'
+   },
+
+   {
+      imgUrl: './img/filipeFisioVR.jpeg',
+      nome: 'Filipe de Lima Namorato',
+      projeto: 'FisioVR',
+      lattesUrl: 'http://lattes.cnpq.br/6818029894846580', 
+      linkedinUrl: 'https://www.linkedin.com/in/filipenamorato'
+   },
+
+   {
+      imgUrl: './img/rayssaElos.jpeg',
+      nome: 'Rayssa Amaral Gomes',
+      projeto: 'ELoS',
+      lattesUrl: 'http://lattes.cnpq.br/3577670358171974', 
+      linkedinUrl: 'https://www.linkedin.com/in/amrlrayssa/'
+   },
+
+   {
+      imgUrl: './img/vitoriaElos.jpeg',
+      nome: 'Vitória César',
+      projeto: 'ELoS',
+      lattesUrl: 'http://lattes.cnpq.br/8023197076103043', 
+      linkedinUrl: 'https://www.linkedin.com/in/vit%C3%B3ria-c%C3%A9sar-4b1060255/'
+   },
+];
+
